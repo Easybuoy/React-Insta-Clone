95a7637da006b506aad6f44c937ba820e5f7218f
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-06-05 14:59:54 +0100

    style login component

--- instagram-app/src/components/Login/Login.jsx
+++ instagram-app/src/components/Login/Login.jsx
@@ -1,28 +1,60 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import "./Login.css";
 
+import appStoreImg from "../../assets/astore.png";
+import googlePlayStoreImg from "../../assets/gplay.png";
+
 export default function Login() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleUsernameChange = (e) => setUsername(e.target.value);
-    const handlePasswordChange = (e) => setPassword(e.target.value);
+  const handleUsernameChange = e => setUsername(e.target.value);
+  const handlePasswordChange = e => setPassword(e.target.value);
 
-    const handleLogin = () => {
-        if (username !== '' && password !== '') {
-            localStorage.setItem('token', username);
-            window.location.reload();
-        }
+  const handleLogin = () => {
+    if (username !== "" && password !== "") {
+      localStorage.setItem("token", username);
+      window.location.reload();
     }
+  };
 
-    return (
-        <div className="login">
-            <h2>Instagram</h2>
-            <div className="login-form">
-                <input onChange={handleUsernameChange} type="text" placeholder="Username" />
-                <input onChange={handlePasswordChange} type="password" placeholder="Password" />
-                <button onClick={handleLogin}>Login</button>
-            </div>
+  return (
+    <>
+      <div className="login">
+        <h2>Instagram</h2>
+        <div className="login-form">
+          <form onSubmit={handleLogin}>
+            <input
+              onChange={handleUsernameChange}
+              type="text"
+              placeholder="Username"
+              required
+            />
+            <input
+              onChange={handlePasswordChange}
+              type="password"
+              placeholder="Password"
+              required
+            />
+            <button onClick={handleLogin}>Log In</button>
+          </form>
+
+          <h6>
+            <a href="##">Forgot Password?</a>
+          </h6>
         </div>
-    )
+      </div>
+
+      <div className="signup">
+        <p>Don't have an account? </p>
+        <a href="##">Sign Up</a>
+      </div>
+
+      <div className="login-footer">
+          <p>Get the App</p>
+        <img src={appStoreImg} alt="App Store" />
+        <img src={googlePlayStoreImg} alt="Google PlayStore Store" />
+      </div>
+    </>
+  );
 }
